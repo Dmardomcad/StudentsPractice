c17c768f0c9389f182f3faa0a77f2dc82d7f2c72
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-10-24 15:30:55 +0200

    fixed tab names.

--- app/src/main/java/com/example/studentspractice/StudentsActivity.kt
+++ app/src/main/java/com/example/studentspractice/StudentsActivity.kt
@@ -1,13 +1,8 @@
 package com.example.studentspractice
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import androidx.viewpager2.widget.ViewPager2
 import com.example.studentspractice.databinding.ActivityStudentsBinding
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 private lateinit var binding: ActivityStudentsBinding
@@ -41,11 +36,10 @@ class StudentsActivity : AppCompatActivity() {
         }.attach()
     }
 
-    private fun getTabTitle(position: Int): String?{
+    private fun getTabTitle(position: Int): String? {
         val students = StudentProvider.studentList
+        val curStudentEmail = students[position].email
 
-        val curStudentName = students[position].name
-        val curStudentSurname = students[position].surname
-        return "${curStudentName}.${curStudentSurname}"
+        return curStudentEmail.substringBefore('@')
     }
 }
