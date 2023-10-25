12ae71a5892e15f9ea8663995badfb5b8b9b0c95
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-10-25 13:38:00 +0200

    Collapsing toolbar added

--- app/src/main/java/com/example/studentspractice/AttendanceActivity.kt
+++ app/src/main/java/com/example/studentspractice/AttendanceActivity.kt
@@ -1,11 +1,25 @@
 package com.example.studentspractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.studentspractice.databinding.ActivityAttendanceBinding
 
 class AttendanceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAttendanceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_attendance)
+
+        binding = ActivityAttendanceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 }
