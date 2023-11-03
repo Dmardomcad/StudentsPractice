25439e3bcb97e8e3d34d567c67126c0410f67404
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-11-03 15:10:22 +0100

    Populate month list

--- app/src/main/java/com/example/studentspractice/AttendanceActivity.kt
+++ app/src/main/java/com/example/studentspractice/AttendanceActivity.kt
@@ -1,15 +1,11 @@
 package com.example.studentspractice
 
 import android.os.Bundle
-import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.studentspractice.databinding.ActivityAttendanceBinding
-import java.text.SimpleDateFormat
 import java.time.LocalDate
-import java.util.Calendar
-import java.util.Locale
 
 class AttendanceActivity : AppCompatActivity() {
 
@@ -24,9 +20,7 @@ class AttendanceActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         setCollapsingBarTitle()
-        setCalendar()
-        //setMonthList()
-        testCalendar()
+        setMonthList()
 
         val toggleButton = binding.fab
 
@@ -36,53 +30,28 @@ class AttendanceActivity : AppCompatActivity() {
         }
     }
 
-    private fun setMonthList(){
-        val monthList = mutableListOf<Month>()
-        val calendar = Calendar.getInstance()
-        val adapterMonth = MonthAdapter(monthList)
-
-
-
-        for(month in Calendar.SEPTEMBER ..Calendar.DECEMBER){
-            calendar.set(Calendar.MONTH, month)
-            val monthName = SimpleDateFormat("MMMM", Locale.getDefault()).format(calendar.time)
-            monthList.add(Month(monthName))
-        }
-    }
-
     private fun setCollapsingBarTitle(){
         val titleName = intent.getStringExtra("name")
         val titleSurname = intent.getStringExtra("surname")
         binding.toolbarLayout.title = "${titleName} ${titleSurname}"
     }
 
-    private fun setCalendar(){
+    private fun setMonthList(){
         val year = 2023
-        for (month in 8..12){
-            val daysOfMonth = MonthProvider.getDaysForMonth(month, year)
-            val monthName = LocalDate.of(year, month, 1).month.name
-            Log.d("Name of month", "${monthName}")
-            daysOfMonth.forEach { day ->
-                Log.d("dia","${day}")
-            }
-        }
-    }
-    private fun testCalendar(){
         val calendarItems = mutableListOf<CalendarItem>()
         val adapterCalendar = CalendarAdapter(calendarItems)
 
         binding.rvMonths.adapter = adapterCalendar
         binding.rvMonths.layoutManager = LinearLayoutManager(this)
 
-        val january = CalendarItem.MonthData("January")
-        val dayOne = CalendarItem.DayData("Monday 1")
-        calendarItems.add(january)
-        calendarItems.add(dayOne)
-        val february = CalendarItem.MonthData("Febrero")
-        val dayTwo = CalendarItem.DayData("Martes 2")
-        calendarItems.add(february)
-        calendarItems.add(dayTwo)
+        for (month in 8..12){
+            val daysOfMonth = MonthProvider.getDaysForMonth(month, year)
+            val monthName = LocalDate.of(year, month, 1).month.name
+            calendarItems.add(CalendarItem.MonthData(monthName))
+            daysOfMonth.forEach { day ->
+                calendarItems.add(CalendarItem.DayData(day))
+            }
+        }
     }
-
 
 }
--- app/src/main/java/com/example/studentspractice/Month.kt
+++
@@ -1,5 +0,0 @@
-package com.example.studentspractice
-
-data class Month (
-    val monthName: String,
-)
