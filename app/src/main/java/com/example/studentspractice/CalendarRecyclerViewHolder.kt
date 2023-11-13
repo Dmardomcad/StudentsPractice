a44b847ddb0930f11c5ca2790334cffd628d92e9
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2023-11-13 10:13:55 +0100

    Text color changes with states of day.

--- app/src/main/java/com/example/studentspractice/CalendarRecyclerViewHolder.kt
+++ app/src/main/java/com/example/studentspractice/CalendarRecyclerViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.studentspractice
 
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import com.example.studentspractice.databinding.RowCalendarDayBinding
@@ -17,6 +18,17 @@ sealed class CalendarRecyclerViewHolder(binding: ViewBinding) : RecyclerView.Vie
             fun bind(dayData: CalendarItem.DayData){
                 binding.txtDay.text = dayData.dayName
                 binding.txtDayState.text = dayData.state
+                changeTextColor(dayData.state)
+            }
+            private fun changeTextColor(state: String) {
+                val colorResId = when (state) {
+                    "Vacaciones" -> R.color.colorStateVacaciones
+                    "Formación" -> R.color.colorStateFormacion
+                    "Centro" -> R.color.colorStateCentro
+                    else -> android.R.color.black
+                }
+                val color = ContextCompat.getColor(binding.root.context, colorResId)
+                binding.txtDayState.setTextColor(color)
             }
         }
     }
